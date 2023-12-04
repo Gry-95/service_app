9f8e839b237a0e7d6a2475c5ea78a1c0869f63f8
Andrey Grinenko <green37tea@gmail.com>
2023-12-04 21:45:16 +0300

    day 1 - create and config serializers

---
+++ service/services/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from services.models import Subscription, Plan
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = '__all__'
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer()
+
+    client_name = serializers.CharField(source='client.company_name')
+    email = serializers.CharField(source='client.user.email')
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance):
+        return instance.price
+        # return (instance.service.full_price -
+        #         instance.service.full_price * (instance.plan.discount_percent / 100))
+
+    class Meta:
+        model = Subscription
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan', 'price')
