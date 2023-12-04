f1e0c506608052828ebd455b48a529cf45ea0d2f
Andrey Grinenko <green37tea@gmail.com>
2023-12-04 19:10:23 +0300

    day 1 - create base models

---
+++ service/services/models.py
@@ -0,0 +1,38 @@
+from django.core.validators import MaxValueValidator
+from django.db import models
+
+from clients.models import Client
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    full_price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+        ('full', 'Full'),
+        ('student', 'Student'),
+        ('discount', 'Discount')
+    )
+
+    plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
+    discount_percent = models.PositiveIntegerField(default=0,
+                                                   validators=[
+                                                       MaxValueValidator(100)
+                                                   ])
+
+    def __str__(self):
+        return f'{self.plan_type}, {self.discount_percent}'
+
+
+class Subscription(models.Model):
+    client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
+    plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.client}, {self.service}'
